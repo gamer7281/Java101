7d2d5f67769778de80e365ac031558f934f2bd82
Umut Aktepe <umut.aktepe@ni.net.tr>
2022-05-06 15:03:16 +0300

    Üs hesaplayan program eklendi

---
+++ UsluSayi/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, k, total = 1;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı : ");
+        n = scan.nextInt();
+
+        System.out.print("Üs olacak sayı : ");
+        k = scan.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            total *= n;
+        }
+
+        System.out.println("Sonuç : " + total);
+    }
+}
